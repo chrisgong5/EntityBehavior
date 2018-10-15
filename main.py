7f0b6dff04e17c38b96ed66f6a3867dbdb135f39
chrisgong5 <chrisgong5@hotmail.com>
2018-10-15 15:58:40 -0500

    main module

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 15:53:23 2018
+
+@author: chris.gong
+
+This is the start point for EntityBehavior project
+"""
+
+def main():
+    print("Hello, world!")
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    
+
